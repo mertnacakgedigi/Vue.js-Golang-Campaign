6c87f3a191021071c86436202039908563073eac
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-10-19 12:40:43 -0700

    server start func fixed

--- backend/main.go
+++ backend/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	r := router.Router()
-	fmt.Println("Starting server on the port 8000...")
 	log.Fatal(http.ListenAndServe(":8000", r))
+	fmt.Println("Starting server on the port 8000...")
+	
 }
